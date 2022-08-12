f1cb295a4778c53755c110f9efc5f461ca63d474
Bean Robinson <bentr99@gmail.com>
2022-08-12 22:16:49 +0100

    added JS for theme buttons

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ if (debug) {
 	$("#topright").css("background-color","blue");
 	$("#botright").css("background-color","green");
 }
+
+const theme = localStorage.getItem("theme");
+if (theme) {
+	$("#body").attr("class", theme);
+}
+
+$("#godark").click(() => {
+	$("#body").attr("class", "dark");
+	localStorage.setItem("theme", "dark");
+});
+
+$("#golight").click(() => {
+	$("#body").attr("class", "light");
+	localStorage.setItem("theme", "light");
+});
