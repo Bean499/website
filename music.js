36953d26ad17b9c0d6c5ab598586822b549812d3
Bean Robinson <bentr99@gmail.com>
2022-08-30 16:39:25 +0100

    new song

--- music.js
+++ music.js
@@ -1,11 +1,22 @@
 const songs = [
 	[
 		"Alone Infection", "28/08/2022",
-		"I'm starting things off with my absolute favourite song, being Faust's theme from Guilty Gear -STRIVE-. This song definitely sounds like it was written by a crazy person. This song's my favourite of all time, and for good reason."
+		`I'm starting things off with my absolute favourite song,
+		being Faust's theme from Guilty Gear -STRIVE-. This song
+		definitely sounds like it was written by a crazy person.
+		This song's my favourite of all time, and for good reason.`
 	],
 	[
 		"Weird Science", "29/08/2022",
-		"This song is very funny. I like how whimsical it is. Expect to see other songs from the album Dead Man's Party, because they're all cracking."
+		`This song is very funny. I like how whimsical it is.
+		Expect to see other songs from the album Dead Man's Party,
+		because they're all cracking.`
+	],
+	[
+		"Zero Stage 2", "30/08/2022",
+		`I enjoy this song very much. The synth is wonderful and I
+		also like the piano, and the clicks are also simply
+		wonderful. As to be expected of the Meagaman X series.`
 	],
 ]
 
