8e4b2c77db5d7069011d5d9422fe01e243eb795e
Bean Robinson <bentr99@gmail.com>
2022-09-01 12:40:32 +0100

    added new song

--- music.js
+++ music.js
@@ -1,25 +1,25 @@
 const songs = [
 	[
-		"Alone Infection", "28/08/2022",
+		"Alone Infection", "GUILTY GEAR -STRIVE-", "28/08/2022",
 		`I'm starting things off with my absolute favourite song,
-		being Faust's theme from Guilty Gear -STRIVE-. This song
+		being Faust's theme from GUILTY GEAR -STRIVE-. This song
 		definitely sounds like it was written by a crazy person.
 		This song's my favourite of all time, and for good reason.`
 	],
 	[
-		"Weird Science", "29/08/2022",
+		"Weird Science", "Dead Man's Party", "29/08/2022",
 		`This song is very funny. I like how whimsical it is.
 		Expect to see other songs from the album Dead Man's Party,
 		because they're all cracking.`
 	],
 	[
-		"Zero Stage 2", "30/08/2022",
+		"Zero Stage 2", "Mega Man X5", "30/08/2022",
 		`I enjoy this song very much. The synth is wonderful and I
 		also like the piano, and the clicks are also simply
 		wonderful. As to be expected of the Meagaman X series.`
 	],
 	[
-		"TSUKI NO SHIHAI", "31/08/2022",
+		"TSUKI NO SHIHAI", "GUILTY GEAR Xrd -REVELATOR-", "31/08/2022",
 		`This is probably my favourite song from Guilty Gear Xrd,
 		tied with Destructive Goodwill (expect to see that song
 		appear soon). I'm a really really big fan of Raven:
@@ -30,10 +30,17 @@ const songs = [
 		for the Revelator character themes because this one's just
 		cracking.`
 	],
+	[
+		"Touch-Tone Telephone", "Spirit Phone", "01/09/2022",
+		`I like this song for reasons similar to Weird Science. The
+		lyrics are very funny and the song overall just appeals to
+		me due to this. I'm not quite sure how to explain why I
+		like it, I just do.`
+	],
 ]
 
 for (i = songs.length - 1; i > -1; i--) {
-	$("#songs").append("<div class='song'><h3>" + songs[i][0] + " - " + songs[i][1] + "</h3><img src='src/img/songs/" + i + ".png' class='cover'><p>" + songs[i][2] + "</p></div>")
+	$("#songs").append("<div class='song'><h3>" + songs[i][0] + " - " + songs[i][2] + "</h3><figure class='cover'><img src='src/img/songs/" + i + ".png'><figcaption>" + songs[i][1] + "</figure><p>" + songs[i][3] + "</p></div>")
 }
 
 $("#recentsong").attr("src", "src/img/songs/" + (songs.length - 1) + ".png");
